a10b3e11bba573c4941d6493d092721753450456
XiaoYouChR <xiaoyouchr@outlook.com>
2026-04-03 21:15:04 +0800

    重构主函数以支持 Slint 平台集成，添加 EspPlatform 结构和相关实现，初始化 Slint 窗口适配器。

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
+use std::rc::Rc;
+use std::time::{Duration, Instant};
+
 use esp_idf_svc::log::EspLogger;
 use log::info;
 
+use slint::platform::software_renderer::{MinimalSoftwareWindow, RepaintBufferType};
+use slint::platform::{Platform, PlatformError};
+
 slint::include_modules!();
 
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+struct EspPlatform {
+    window: Rc<MinimalSoftwareWindow>,
+    start: Instant,
+}
 
-    // Bind the log crate to the ESP Logging facilities
+impl Platform for EspPlatform {
+    fn create_window_adapter(
+        &self,
+    ) -> Result<Rc<dyn slint::platform::WindowAdapter>, PlatformError> {
+        Ok(self.window.clone())
+    }
+
+    fn duration_since_start(&self) -> Duration {
+        self.start.elapsed()
+    }
+}
+
+fn main() {
+    esp_idf_svc::sys::link_patches();
     EspLogger::initialize_default();
-    
-    let app = App::new().unwrap();
+
+    info!("Initializing Slint platform");
+
+    let platform = EspPlatform {
+        window: MinimalSoftwareWindow::new(RepaintBufferType::NewBuffer),
+        start: Instant::now(),
+    };
+
+    slint::platform::set_platform(Box::new(platform)).expect("failed to set Slint platform");
+
+    info!("Starting Slint integration test");
+
+    let app = App::new().expect("failed to create Slint app");
     app.set_counter(1);
 
     info!("Slint UI object created successfully");
